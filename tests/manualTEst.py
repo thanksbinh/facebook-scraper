4cf6de480667f765ed11ca04174614fef1b20c89
Moda20 <kadhem03@gmail.com>
2023-11-25 16:03:14 +0100

    add manual test

--- tests/manualTEst.py
+++ tests/manualTEst.py
@@ -2,5 +2,5 @@ import json
 
 from facebook_scraper import get_posts
 
-post = next(get_posts(account=100089065833006, start_url="https://mbasic.facebook.com/HautStandingImmobilier?v=timeline", cookies='../cks.txt'))
+post = next(get_posts(account=100089065833006, start_url="https://mbasic.facebook.com/<pageId>?v=timeline", cookies='../cks.txt'))
 print(post['images'])
